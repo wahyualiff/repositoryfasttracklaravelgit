dfe05650c16c924ba2f119621c230eb7dd7661ef
wahyualif <wahyualief1@gmail.com>
2021-06-16 15:11:35 +0700

    menambahkan slider pada halaman user

--- resources/views/motogp.blade.php
+++ resources/views/motogp.blade.php
@@ -8,6 +8,7 @@
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
@@ -18,6 +19,9 @@
       </div>
       <div class="carousel-item">
         <img src="{{asset('storage/sl3.jpg')}}" class="d-block w-100" alt="Third Slide">
+      </div>
+      <div class="carousel-item">
+        <img src="{{asset('storage/sl4.jpg')}}" class="d-block w-100" alt="Fourth Slide">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
